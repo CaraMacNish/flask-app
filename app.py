f6a06b6748d621515bae5f31049dbab2131ca639
CaraMacNish <58584776+CaraMacNish@users.noreply.github.com>
2021-07-07 19:56:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ from flask import Flask
 #from flask_pymongo import PyMongo
 from flask import render_template, flash, redirect, url_for
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://cara:gumpt10n@cluster0.gbmri.mongodb.net/workout?retryWrites=true&w=majority")
-db=client.admin
+#client = MongoClient("mongodb+srv://cara:gumpt10n@cluster0.gbmri.mongodb.net/workout?retryWrites=true&w=majority")
+#db=client.admin
 # Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-print(serverStatusResult)
+#serverStatusResult=db.command("serverStatus")
+#print(serverStatusResult)
 
-db = client.workout
-count = db.members.count_documents({})
+#db = client.workout
+#count = db.members.count_documents({})
           
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', count = count)
+    return render_template('index.html')
+#    return render_template('index.html', count = count)
