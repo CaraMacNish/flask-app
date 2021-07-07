99978e671b29aaff020159eb2d71b97da1d9aab4
CaraMacNish <58584776+CaraMacNish@users.noreply.github.com>
2021-07-07 20:13:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 #from flask_pymongo import PyMongo
 from flask import render_template, flash, redirect, url_for
 from pymongo import MongoClient
-#client = MongoClient("mongodb+srv://cara:gumpt10n@cluster0.gbmri.mongodb.net/workout?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://cara:gumpt10n@cluster0.gbmri.mongodb.net/workout?retryWrites=true&w=majority")
 #db=client.admin
 # Issue the serverStatus command and print the results
 #serverStatusResult=db.command("serverStatus")
