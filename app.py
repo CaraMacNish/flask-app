bf8a21905127d6de137bfe7d7b29f26f4529026f
CaraMacNish <58584776+CaraMacNish@users.noreply.github.com>
2021-07-08 09:50:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ client = MongoClient("mongodb+srv://cara:gumpt10n@cluster0.gbmri.mongodb.net/wor
           
 app = Flask(__name__)
 
-#db = client.workout
+db = client.workout
 #count = db.members.count_documents({})
 count = 4
 
